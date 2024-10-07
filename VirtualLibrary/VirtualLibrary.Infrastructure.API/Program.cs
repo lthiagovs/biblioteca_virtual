ac2d470287be7ad1184bd6a3366c09d97680b9a3
= <lthiagofarias@gmail.com>
2024-10-06 21:46:02 -0300

    feat: api data context connection

--- VirtualLibrary/VirtualLibrary.Infrastructure.API/Program.cs
+++ VirtualLibrary/VirtualLibrary.Infrastructure.API/Program.cs
@@ -1,11 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using VirtualLibrary.Infrastructure.DataContext.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
