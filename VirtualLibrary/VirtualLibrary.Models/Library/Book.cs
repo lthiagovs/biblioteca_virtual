eac4115a448d7adc90652d68e282c39a6cf1d377
= <lthiagofarias@gmail.com>
2024-10-06 21:20:19 -0300

    feat: book model feat: user model

---
+++ VirtualLibrary/VirtualLibrary.Models/Library/Book.cs
@@ -0,0 +1,23 @@
+﻿using VirtualLibrary.Domain.Models.Person;
+
+namespace VirtualLibrary.Domain.Models.Library
+{
+    public class Book
+    {
+
+        public int ID { get; set; }
+
+        public string Title {  get; set; } = string.Empty;
+
+        public string Description {  get; set; } = string.Empty;
+
+        public string Category {  get; set; } = string.Empty;
+
+        public int AuthorID { get; set; }
+
+        public User? Author {  get; set; }
+        
+
+    }
+
+}
---
+++ VirtualLibrary/VirtualLibrary.Models/Person/User.cs
@@ -0,0 +1,20 @@
+﻿namespace VirtualLibrary.Domain.Models.Person
+{
+    public class User
+    {
+
+        public int ID { get; set; }
+
+        public string Name { get; set; } = String.Empty;
+
+        public string Email { get; set; } = String.Empty;
+
+        public string Password {  get; set; } = String.Empty;
+
+        public string Description {  get; set; } = String.Empty;
+
+        public string PhoneNumber { get; set; } = String.Empty;
+
+    }
+
+}
