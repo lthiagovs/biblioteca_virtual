f6ae003e301ee944f207aabb156badc067517e8c
= <lthiagofarias@gmail.com>
2024-10-06 21:22:08 -0300

    feat: data annotations

--- VirtualLibrary/VirtualLibrary.Models/Library/Book.cs
+++ VirtualLibrary/VirtualLibrary.Models/Library/Book.cs
@@ -1,18 +1,26 @@
-﻿using VirtualLibrary.Domain.Models.Person;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using VirtualLibrary.Domain.Models.Person;
 
 namespace VirtualLibrary.Domain.Models.Library
 {
     public class Book
     {
 
+        [Key]
         public int ID { get; set; }
 
+        [Required]
         public string Title {  get; set; } = string.Empty;
 
+        [Required]
+        [MaxLength(1000)]
         public string Description {  get; set; } = string.Empty;
 
+        [Required]
         public string Category {  get; set; } = string.Empty;
 
+        [ForeignKey(nameof(Author))]
         public int AuthorID { get; set; }
 
         public User? Author {  get; set; }
--- VirtualLibrary/VirtualLibrary.Models/Person/User.cs
+++ VirtualLibrary/VirtualLibrary.Models/Person/User.cs
@@ -1,18 +1,28 @@
-﻿namespace VirtualLibrary.Domain.Models.Person
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VirtualLibrary.Domain.Models.Person
 {
     public class User
     {
 
+        [Key]
         public int ID { get; set; }
 
+        [Required]
         public string Name { get; set; } = String.Empty;
 
+        [Required]
         public string Email { get; set; } = String.Empty;
 
+        [Required]
         public string Password {  get; set; } = String.Empty;
 
+        [Required]
+        [MaxLength(1000)]
         public string Description {  get; set; } = String.Empty;
 
+        [Required]
+        [MaxLength(11)]
         public string PhoneNumber { get; set; } = String.Empty;
 
     }
