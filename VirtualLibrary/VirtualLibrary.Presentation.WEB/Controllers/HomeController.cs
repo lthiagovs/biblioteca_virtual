94cf10b012b18ddee3efc277a7f1f581f27f16de
= <lthiagofarias@gmail.com>
2024-10-09 20:22:09 -0300

    design: mvc page layout

--- VirtualLibrary/VirtualLibrary.Presentation.WEB/Controllers/HomeController.cs
+++ VirtualLibrary/VirtualLibrary.Presentation.WEB/Controllers/HomeController.cs
@@ -1,16 +1,14 @@
 using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
-using VirtualLibrary.Presentation.WEB.Models;
 
 namespace VirtualLibrary.Presentation.WEB.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+
+        public HomeController()
         {
-            _logger = logger;
+            
         }
 
         public IActionResult Index()
@@ -18,15 +16,8 @@ namespace VirtualLibrary.Presentation.WEB.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+
+
     }
 }
