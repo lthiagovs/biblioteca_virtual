510c0f29d4fc109a6e7e431670e88cb436281a43
= <lthiagofarias@gmail.com>
2024-10-09 19:27:00 -0300

    feat: user dto

---
+++ VirtualLibrary/VirtualLibrary.Domain.Dto/Person/UserDto.cs
@@ -0,0 +1,16 @@
+﻿namespace VirtualLibrary.Domain.Dto.Person
+{
+    public class UserDto
+    {
+
+        public int ID {  get; set; }
+
+        public string Name { get; set; } = String.Empty;
+
+        public string Email { get; set; } = String.Empty;
+
+        public string Description {  get; set; } = String.Empty;
+
+    }
+
+}
---
+++ VirtualLibrary/VirtualLibrary.Infrastructure.API/Controllers/UserController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VirtualLibrary.Infrastructure.API.Interfaces;
+
+namespace VirtualLibrary.Infrastructure.API.Controllers
+{
+    [ApiController]
+    public class UserController : Controller
+    {
+
+        private readonly IUserRepository _userRepository;
+
+        public UserController(IUserRepository userRepository)
+        {
+            this._userRepository = userRepository;
+        }
+
+
+    }
+}
